a4ea746bc37e547aa5e9ebd75c46bacd06416260
maulanasyarif <maulanasyarif127@gmail.com>
2022-11-17 02:12:25 +0700

    dashoard

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -3,12 +3,22 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\M_Subkriteria;
+use App\Models\M_Kriteria;
+use App\Models\M_Karyawan;
 
 class Auth extends BaseController
 {
     public function index()
     {
-        return view('admin/dashboard');
+        $karyawan = new M_Karyawan();
+        $kriteria = new M_Kriteria();
+        $subkriteria = new M_Subkriteria();
+        $data['kriteria'] = $kriteria->countAll();
+        $data['subkriteria'] = $subkriteria->countAll();
+        $data['karyawan'] = $karyawan->countAll();
+
+        return view('admin/dashboard', $data);
     }
 
     public function users()
--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -4,29 +4,26 @@
     <div class="container">
         <div class="row row-cols-1 row-cols-md-3 g-4">
             <div class="col">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
+                <div class="card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h5 class="card-title">Total Kriteria</h5>
+                        <h3><?= $kriteria; ?></h3>
                     </div>
                 </div>
             </div>
             <div class="col">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
+                <div class="card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h5 class="card-title">Total Subkriteria</h5>
+                        <h3><?= $subkriteria ?></h3>
                     </div>
                 </div>
             </div>
             <div class="col">
-                <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
+                <div class="card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                        <h5 class="card-title">Total Pegawai</h5>
+                        <h3><?= $karyawan ?></h3>
                     </div>
                 </div>
             </div>
